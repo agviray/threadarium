0a847f0c88abef9982ceb2670f3f8fc0fcb35d11
Allan Viray <allang.v.dev@gmail.com>
2024-04-30 18:04:20 -0500

    Display an individual details page for every clicked post

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('posts/', views.posts_index, name='index'),
+  path('posts/<int:post_id>/', views.posts_detail, name='detail'),
   path("__reload__/", include("django_browser_reload.urls")),
 ]
