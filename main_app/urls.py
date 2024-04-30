dbb1f1649b781ec13127eb75ca1694a0edc314b3
Allan Viray <allang.v.dev@gmail.com>
2024-04-30 16:51:29 -0500

    Render list of temporary user posts
    - Set up url, UI, views, and template, for user's posts.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('posts/', views.posts_index, name='index'),
   path("__reload__/", include("django_browser_reload.urls")),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 
+posts = [
+  {
+    'title': 'Post One',
+    'body': 'This is post number 1'
+  },
+  {
+    'title': 'Post Two',
+    'body': 'This is post number 2'
+  },
+  {
+    'title': 'Post Three',
+    'body': 'This is post number 3'
+  },
+  {
+    'title': 'Post Four',
+    'body': 'This is post number 4'
+  },
+]
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def posts_index(request):
+  return render(request, 'posts/index.html', {
+    'posts': posts
+  })
