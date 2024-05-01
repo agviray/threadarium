e909f779f1a748527d8882cdf58e6d6683e3eb90
Allan Viray <allang.v.dev@gmail.com>
2024-04-30 21:32:25 -0500

    Add get_absolute_url method to Post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
   title = models.CharField(max_length=100)
@@ -6,3 +7,6 @@ class Post(models.Model):
 
   def __str__(self):
     return self.title
+  
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'post_id': self.id})
