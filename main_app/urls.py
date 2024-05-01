e5c276e0a5d2742f629b6ee59601d46b3b6a9200
Allan Viray <allang.v.dev@gmail.com>
2024-05-01 18:25:10 -0500

    Wip: Add sign up feature for new users
    - Add url pattern for sign up functionality.
    - Write signup view function.

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
   path('posts/create/', views.PostCreate.as_view(), name='post_create'),
   path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
   path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
+  path('accounts/signup/', views.signup, name='signup'),
   path("__reload__/", include("django_browser_reload.urls")),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # Create 'user' form objectd that includes
+    # data from the browser.
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # Add user to database
+      user = form.save()
+      # Log user in with:
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or GET request results in rendering signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
 def home(request):
   return render(request, 'home.html')
 
