8efbf45027dcbc31dfbe27fdf9b7a6fc1e07f57a
Allan Viray <allang.v.dev@gmail.com>
2024-05-01 16:22:25 -0500

    Create one-to-many relationship between User and Post

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
   title = models.CharField(max_length=100)
   body = models.TextField(max_length=250)
-
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
   def __str__(self):
     return self.title
   
