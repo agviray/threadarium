b98b2754d435452cdc43f9698de21304270daf68
Allan Viray <allang.v.dev@gmail.com>
2024-05-01 20:09:02 -0500

    Implement authorization on views

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def signup(request):
   error_message = ''
@@ -29,19 +31,21 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def posts_index(request):
   posts = Post.objects.filter(user=request.user)
-  
+
   return render(request, 'posts/index.html', {
     'posts': posts
   })
 
+@login_required
 def posts_detail(request, post_id):
   post = Post.objects.get(id=post_id)
 
   return render(request, 'posts/detail.html', {'post': post})
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
   model = Post
   fields = ['title', 'body']
 
@@ -54,10 +58,10 @@ class PostCreate(CreateView):
 
     return super().form_valid(form)
 
-class PostUpdate(UpdateView):
+class PostUpdate(LoginRequiredMixin, UpdateView):
   model = Post
   fields = ['body']
 
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin, DeleteView):
   model = Post
   success_url = '/posts'
