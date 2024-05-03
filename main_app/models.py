f9020a343a4b7cc68b2656eee4f014da0cc207ed
Allan Viray <allang.v.dev@gmail.com>
2024-05-02 23:45:17 -0500

    Render all posts in descending order

--- main_app/models.py
+++ main_app/models.py
@@ -9,11 +9,15 @@ class Post(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   created_on = models.DateTimeField(auto_now_add=True)
   
+  class Meta:
+    ordering = ['-created_on']
+  
   def __str__(self):
     return self.title
   
   def get_absolute_url(self):
     return reverse('detail', kwargs={'post_id': self.id})
+  
   
 class Comment(models.Model):
   body = models.TextField(250)
