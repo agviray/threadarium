920d2ac6b26367c77fcbdfb09716939d92ee12f4
Allan Viray <allang.v.dev@gmail.com>
2024-05-02 23:36:02 -0500

    Add username and comment time detail on comments left under a post

--- main_app/models.py
+++ main_app/models.py
@@ -21,3 +21,5 @@ class Comment(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   created_on = models.DateTimeField(auto_now_add=True)
 
+  class Meta:
+    ordering = ['-created_on']
