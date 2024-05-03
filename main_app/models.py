dbdcc59eca82d55ac2afb92072e47ec0e554b705
Allan Viray <allang.v.dev@gmail.com>
2024-05-02 22:49:47 -0500

    Add created_on fields to Post and Comment models and update detail template

--- main_app/models.py
+++ main_app/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
   title = models.CharField(max_length=100)
   body = models.TextField(max_length=250)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_on = models.DateTimeField(auto_now_add=True)
   
   def __str__(self):
     return self.title
@@ -18,4 +19,5 @@ class Comment(models.Model):
   body = models.TextField(250)
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_on = models.DateTimeField(auto_now_add=True)
 
